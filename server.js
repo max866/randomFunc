7888f401a8c0c84aff4898f3e6c5f218de3ccedd
max866 <max866.hung@gmail.com>
2017-09-02 23:36:38 +0800

    add modules folder and random function

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var random = require('./modules/module1.js');
+
 app.get('/',function(req,res){
-    res.send('Testing the route');
+    //res.send('Testing the route');
+    res.send("Random Characters:" + random.RandomChar(40));
 });
 
 app.listen(1234,function(){
